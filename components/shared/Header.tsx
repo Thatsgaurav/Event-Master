4c2cec27d851b1069dffd599fd9567be7b06d3bd
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2024-04-06 00:36:51 +0530

    commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,38 +6,38 @@ import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
 
 const Header = () => {
-    return (
-        <header className="w-full border-b">
-            <div className="wrapper flex items-center justify-between">
-                <Link href="/" className="w-36">
-                    <Image
-                        src="/assets/images/logo.jpg" width={128} height={38}
-                        alt="Evently logo"
-                    />
-                </Link>
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Eventl Master Logo"
+          />
+        </Link>
 
-                <SignedIn>
-                    <nav className="md:flex-between hidden w-full max-w-xs">
-                        <NavItems />
-                    </nav>
-                </SignedIn>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
-                <div className="flex w-32 justify-end gap-3">
-                    <SignedIn>
-                        <UserButton afterSignOutUrl="/" />
-                        <MobileNav />
-                    </SignedIn>
-                    <SignedOut>
-                        <Button asChild className="rounded-full bg-black hover:bg-green-700" size="lg">
-                            <Link href="/sign-in">
-                                Login
-                            </Link>
-                        </Button>
-                    </SignedOut>
-                </div>
-            </div>
-        </header>
-    )
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full bg-black hover:bg-green-700" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
